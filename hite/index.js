f3c260362a288e7b9ac89dfa749ef72fd57e99c2
mahotich <rmahotka@gmail.com>
2022-06-02 21:04:15 +0300

    feat:fix bags

--- hite/index.js
+++ hite/index.js
@@ -4,4 +4,5 @@ let menuBurger= document.querySelector('.menu__nav');
 burgerBtn.addEventListener('click', function(){
   menuBurger.classList.toggle('active');
   burgerBtn.classList.toggle('burger_active');
+  document.body.classList.toggle('body_active');
 });
